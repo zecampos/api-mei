b1260259fc88d24270d19818312c0ed801fe632f
Jose Guilherme <joseguilhermesantoscampos@gmail.com>
2019-11-06 20:57:23 -0300

    add plano

--- database/migrations/1573039909565_usuario_schema.js
+++ database/migrations/1573039909565_usuario_schema.js
@@ -15,6 +15,7 @@ class UsuarioSchema extends Schema {
       table.string('situacao', 254)
       table.string('municipio', 254)
       table.string('fantasia', 254)
+      table.string('plano', 254).defaultTo('free')
       table.string('cnpj', 254).unique()
       table.timestamps()
     })
